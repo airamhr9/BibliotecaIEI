8fbc29dd57b28337eb78f3db4018432603ed1db5
Hector Alcaide <hecalrol@outlook.com>
2021-11-22 01:36:11 +0100

    Obtener coordenadas con selenium (funciona solo a veces xd)

--- src/main/kotlin/extractors/Ubicacion.kt
+++ src/main/kotlin/extractors/Ubicacion.kt
@@ -3,10 +3,6 @@ package extractors
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.support.ui.ExpectedConditions
-import org.openqa.selenium.support.ui.WebDriverWait
-import java.time.Duration
-
 
 class Ubicacion(driverPath: String) {
 
@@ -23,13 +19,16 @@ class Ubicacion(driverPath: String) {
         campoDireccion.sendKeys(direccion)
         val botonEnviar = driver.findElement(By.cssSelector("button[onclick='codeAddress()']"))
         botonEnviar.click()
-        /*val waiting = WebDriverWait(driver, Duration.ofSeconds(10))
-        waiting.until()*/
         val campoLatitud = driver.findElement(By.id("latitude"))
-        val latitud = campoLatitud.text //No tiene texto
+        val latitud = campoLatitud.getAttribute("value")
         val campoLongitud = driver.findElement(By.id("longitude"))
-        val longitud = campoLongitud.text //No tiene texto
+        val longitud = campoLongitud.getAttribute("value")
+        driver.close()
         return PuntoGeografico(latitud.toDouble(), longitud.toDouble())
     }
+
+    /* fun cerrarNavegador() {
+        driver.close()
+    } */
 
 }
